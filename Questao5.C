290529281152736cf5da28edf841cd301e249b32
MarcoWilhelm1 <121198525+MarcoWilhelm1@users.noreply.github.com>
2023-04-18 21:30:51 -0300

    Add files via upload

--- Questao5.C
+++ Questao5.C
@@ -4,9 +4,7 @@ int main(){
     int x; // declaração de variaveis
     printf("Digite um numero: ");
     scanf("%d", &x); // inserindo valor nas variaveis
-    if(x % 2 == 0) // condição se for par ou impar
-        printf("O valor e par"); // imprimindo o resultado
-    else
-        printf("O valor e impar"); // imprimindo o resultado
+    printf("O antecessor do numero e: %d", x-1); // imprimindo o resultado
+    printf("\nO sucessor do numero e: %d", x+1); // imprimindo o resultado
     return 0;
 }
--- Questao9.C
+++ Questao9.C
@@ -4,9 +4,8 @@ int main(){
     int x; // declaração de variaveis
     printf("Digite um numero: ");
     scanf("%d", &x); // inserindo valor nas variaveis
-    if(x % 2 == 0) // condição se for par ou impar
-        printf("O valor e par"); // imprimindo o resultado
-    else
-        printf("O valor e impar"); // imprimindo o resultado
+    printf("Tabuada de %d\n", x);
+    for(int i = 1; i<=10; i++) // laço de repetição para fazer a tabuada
+        printf("%d * %d = %d\n", x, i, x*i); // imprimindo o resultado
     return 0;
 }
